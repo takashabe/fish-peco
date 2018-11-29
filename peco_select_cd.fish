8ce4b86e54c75ffc0df90aee514ca110c0c782ad
takashabe <abe_takashi@cyberagent.co.jp>
2018-11-29 12:13:35 +0900

    Add default args

--- peco_select_cd.fish
+++ peco_select_cd.fish
@@ -4,8 +4,19 @@ function peco_select_cd
     set peco_flags --query "$query"
   end
 
-  set -l ignore $PECO_SELECT_CD_IGNORE_DIR
-  find . -maxdepth 3 -type d | egrep -v $ignore | peco $peco_flags | read line
+  set -l max_depth $PECO_SELECT_CD_MAX_DEPTH
+  set -l ignore_case $PECO_SELECT_CD_IGNORE_CASE
+
+  if test -z $max_depth
+    set max_depth 3
+  end
+
+  if test -z $ignore_case
+    find . -maxdepth $max_depth -type d | peco $peco_flags | read line
+  else
+    find . -maxdepth $max_depth -type d | egrep -v $ignore_case | peco $peco_flags | read line
+  end
+
   if test $line
     cd $line
     commandline -f repaint
